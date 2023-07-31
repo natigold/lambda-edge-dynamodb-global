7ec59be6e8ca94bb04ef58f33366e061656331c0
natigold <natigold@gmail.com>
2023-07-31 11:35:54 +0300

    Added latency based connection to DynamoDB

--- src/handlers/router-function/app.mjs
+++ src/handlers/router-function/app.mjs
@@ -1,47 +1,63 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-
 import { GetItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import * as f from 'util'
+import dns from 'dns/promises'
 
 const TABLE_NAME = 'RoutingUrlTable';
+const DEFAULT_REGION = 'us-east-1';
+const REGIONS = ['us-east-1', 'eu-west-1', 'ap-southeast-1', 'eu-central-1'];
 const REGION = process.env.AWS_REGION;
-let ddbClient = new DynamoDBClient({ region: REGION });
+
+let domainName;
+let ddbClient = new DynamoDBClient({ region: REGIONS.indexOf(REGION) > -1 ? REGION : REGION });
 
 export const lambdaHandler = async (event, context, callback) => {
 
-    console.log("event: ", event);
-    console.log("context: ", context);
-
     const request = event.Records[0].cf.request;
     console.log("cf request: ", request);
+
+    // Get a latency based connection to DynamoDB only if the current region isn't supported
+    // or current connection is invalid 
+    if (!ddbClient || (!domainName && REGIONS.indexOf(REGION) == -1)) {
+      domainName = request.headers['host'][0].value;
+      console.log("Setting domain: ", domainName);
+
+      ddbClient = getDynamoDBConnection();
+    }
 
     const searchParams = new URLSearchParams(request.querystring);
 
     if (searchParams.has('url') && searchParams.has('type')) {
       console.log("url and type found: ", searchParams.get('url'), searchParams.get('type'));
 
+      // Measure the latency of calling DynamoDB Global table
       const timestamp1 = new Date().getTime();
       const item = await getRecordByUrlAndType(searchParams.get('url'), searchParams.get('type'));
       const timestamp2 = new Date().getTime();
       const elapseTime = timestamp2 - timestamp1;
-      console.log("time taken: ", elapseTime);
+      console.log("DynamoDB latency (ms): ", elapseTime);
 
+      // If item was found check for redirection settings
       if (item && item.Item) {
+
+        // If status is active, redirect to the URL
+        if (item.Item.status.BOOL) {
+          const response = {
+            status: '302',
+            statusDescription: 'Found',
+            headers: {
+                location: [{
+                    key: 'Location',
+                    value: item.Item.url.S,
+                }],
+            },
+          };
+
+          console.log('Redirecting to: ', item.Item.url.S);
+
+          callback(null, response);
+        }
+
         request.headers['x-route-item'] = [{key: 'X-Route-Item', value: JSON.stringify(item.Item)}]; 
-        request.headers['x-route-latency'] = [{key: 'X-Route-Latency', value: elapseTime.toString()}]; 
-        
-        console.log("header: ", request.headers);
+        console.log("Added headers: ", request.headers);
       }
     }
 
@@ -60,10 +76,6 @@ const getRecordByUrlAndType = async (url, type) => {
     }
   };
 
-  if (!ddbClient) {
-    ddbClient = new DynamoDBClient({ region: REGION });
-  }
-
   const command = new GetItemCommand(params);
 
   try {
@@ -75,4 +87,30 @@ const getRecordByUrlAndType = async (url, type) => {
   }
 
   return item;
-}
+};
+
+const getDynamoDBConnection = async () => {
+  let region = DEFAULT_REGION;
+
+  if (REGIONS.indexOf(REGION) > -1) {
+    region = REGION;
+  } else {
+    region = domainName ? await getLatencyRegion(domainName) : DEFAULT_REGION;
+  }
+  console.log("Connecting to DynanmoDB in region: ", region);
+
+  return new DynamoDBClient({ region: region });
+};
+
+const getLatencyRegion = async (domainName) => {
+  let lowestLatencyRegion = DEFAULT_REGION;
+
+  try {
+    const res = await dns.resolveTxt(domainName);
+    lowestLatencyRegion = res[0][0]
+  } catch (err) {
+    console.log("Error retrieving latency record", err);
+  }
+
+  return lowestLatencyRegion;
+};
